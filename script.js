75aecba419956eb19776c820cd7da49764e099d9
Cindel Sousa <cindelevelyn@gmail.com>
2022-06-13 11:29:03 -0300

    mudei o carrosel

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function Enviar() {
+function Cadastro() {
+    var marca = document.getElementById("marca_escolhida");
+    var ano = document.getElementById("ano_escolhido");
+    var modelo = document.getElementById("modelo_carro");
+    var preco = document.getElementById("preco");
+    var foto = document.getElementById("foto");
+    var cor = document.getElementById("cor_escolhida");
 
-    var cadastro = document.getElementById("modelo_carro");
+    carro = {marca: marca.value,ano: ano.value,modelo: modelo.value,preco: preco.value,foto: foto.value,cor: cor.value};
 
-    if (cadastro.value != "") {
-        alert('Obrigado sr(a) ' + cadastro.value + ' os seus dados foram encaminhados com sucesso');
-    }
+    console.log(carro);
+}
 
+function ColocaNaLista(carro)
+{
+    console.log(carro.marca);
+    //document.getElementById("marca_do_carro").innerHTML = carro.marca;
 }
