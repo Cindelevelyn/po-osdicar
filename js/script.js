5148cad230fe1aa9f750fead69f558329f36f71b
Cindel Sousa <cindelevelyn@gmail.com>
2022-06-20 14:00:05 -0300

    terminei

--- js/script.js
+++ js/script.js
@@ -1,11 +1,49 @@
-var a = prompt("Digite seu nome: ", "Nome");
-var b = prompt("Digite seu sobrenome: ", "Sobrenome");
-document.write("Bom dia " + a + " " + b);
+document
+  .getElementById("btn_cadastro")
+  .addEventListener("click", validar, false);
 
-var i = confirm("Deseja continuar?");
-if (i == true) {
-    document.write("\nAceitou");
-}
-else {
-    document.write("\nNegou");
+function validar() {
+  var $divMensagemModelo = document.getElementById("divMensagemModelo");
+  var $divMensagemMarca = document.getElementById("divMensagemMarca");
+  var $divMensagemAno = document.getElementById("divMensagemAno");
+  var $divMensagemPreco = document.getElementById("divMensagemPreco");
+  var $divMensagemFoto = document.getElementById("divMensagemFoto");
+  var $divMensagemCor = document.getElementById("divMensagemCor");
+  var $divMensagemDesc = document.getElementById("divMensagemDesc");
+  var $Modelo = document.getElementById("modelo_carro");
+  var $Marca = document.getElementById("marca_escolhida");
+  var $Ano = document.getElementById("ano_escolhido");
+  var $Preco = document.getElementById("preco");
+  var $Foto = document.getElementById("foto");
+  var $Cor = document.getElementById("cor_escolhida");
+  var $Desc = document.getElementById("descricao");
+
+  if ($Modelo.value.length < 2)
+    $divMensagemModelo.innerHTML = "O modelo deve ser informado";
+  else $divMensagemModelo.innerHTML = "";
+
+  if ($Marca.value == "-- Marca")
+    $divMensagemMarca.innerHTML = "A marca deve ser informado";
+  else $divMensagemMarca.innerHTML = "";
+
+  if ($Ano.value == "-- Ano")
+    $divMensagemAno.innerHTML = "O ano deve ser informado";
+  else $divMensagemAno.innerHTML = "";
+
+  if ($Preco.value == 'R$ 0,00' || $Preco.value == 0)
+    $divMensagemPreco.innerHTML = "O preço deve ser informado";
+  else $divMensagemPreco.innerHTML = "";
+  
+  if ($Foto.value.length == 0)
+    $divMensagemFoto.innerHTML = "A foto deve ser informada";
+  else $divMensagemFoto.innerHTML = "";
+
+  if ($Cor.value == "-- Cor")
+    $divMensagemCor.innerHTML = "A cor deve ser informada";
+  else $divMensagemCor.innerHTML = "";
+
+  if ($Desc.value.length < 20)
+    $divMensagemDesc.innerHTML = "Descrição muito pequena";
+  else $divMensagemDesc.innerHTML = "";
+
 }
